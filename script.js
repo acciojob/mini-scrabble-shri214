2fd5b4f23f3ea24dd1506a2246726ed770d4b6ad
Shriram kushwaha <117708615+shri214@users.noreply.github.com>
2023-04-15 21:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+//your cod
+var inputs=document.querySelector("#evaluatedText");
+inputs.addEventListener("input", ()=>{
+	let len=document.querySelector("#evaluatedText").value.length;
+	document.getElementById("letterCount").innerText=len;
+})
